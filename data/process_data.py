eaa427b06871c61b0b3a8f6ca7e55757f43d4fb9
Nongnooch Roongpiboonsopit <nongnoochr@gmail.com>
2019-02-24 17:05:41 -0500

    Commit process_data.py
    Implement "process_data.py" for the ETL pipeline
    
    Also commit the output db that will be used in the ML pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,99 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Load the specified message and categories data and merge them using the 'id'
+    column. The output is a merged data frame.
+
+    Args:
+    messages_filepath: string. A relative path to the message data file.
+    categories_filepath: string. A relative path to the categories data file.
+
+    Returns:
+    df: dataframe. A merged data frame.
+
+    '''
+    
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = messages.merge(categories, on='id', how='outer')
+    return df
+    
+    
 
 
 def clean_data(df):
-    pass
+    '''
+    Clean the categories data of the specified dataframe
+
+    Args:
+    df: dataframe. Merged dataframe before cleaning.
+
+    Returns:
+    df: dataframe. The cleaned dataframe.
+
+    '''
+
+    # === Split categories into separate category columns
+
+    # create a dataframe of the 36 individual category columns
+    categories = df['categories'].str.split(';', expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0, :]
+
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.str.split('-', expand=True)[0]
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # === Convert category values to just numbers 0 or 1
+    for column in category_colnames:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].str.split('-', expand=True)[1]
+        
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+
+    # === Replace categories column in df with new category columns
+
+    # drop the original categories column from `df`
+    df = df.drop(['categories'], axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # === Remove duplicates
+
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    '''
+    Save the dataframe to a sqlite file.
+
+    Args:
+    df: dataframe. Cleaned dataframe with data to be saved to a sqlite file
+    database_filename: string. Sqlite file name
+
+    '''
+    
+    engine = create_engine('sqlite:///{}'.format(database_filename))
+    engine.execute('DROP TABLE IF EXISTS InsertTableName')
+    df.to_sql('InsertTableName', engine, index=False)
 
 
 def main():

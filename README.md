9fe5cc24105ff076776abf68efb954af4db93a80
Nongnooch Roongpiboonsopit <nongnoochr@gmail.com>
2019-02-26 23:06:02 -0500

    Update README and add the visualization code to the web app

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 # Disaster Response Pipeline Project
 
+This project includes a web app that is using a Machine Learning model that will help an emergency worker to classify a message during diaster events in several categories and it also shows visualizations to show insights of the collecting data that is used to train the Machine Learning model.
+
+This project also contains python scripts that is used in the ETL (Extract, Transform, and Load) and machine learning pipelines which generate a sqlite file that contains data after performing the ETL pipeline and a pickle file which stores a trained Machine Learning model that is used in the web app.
+
+ 
+
+### Installation
+Below are python libraries that are required to run this code using Python versions 3.*:
+
+* numpy
+* pandas
+* sqlalchemy
+* nltk
+* sklearn
+
 ### Instructions:
 1. Run the following commands in the project's root directory to set up your database and model.
 
@@ -12,3 +27,10 @@
     `python run.py`
 
 3. Go to http://0.0.0.0:3001/
+
+### Licensing, Authors, Acknowledgements
+
+Data files used in this project were from [Figure Eight](https://www.figure-eight.com/).
+You can find the Licensing for the data and other descriptive information there.
+
+This project is [MIT licensed](./LICENSE).
--- app/run.py
+++ app/run.py
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Scatter
 from sklearn.externals import joblib
 from sqlalchemy import create_engine
 
@@ -26,11 +26,11 @@ def tokenize(text):
     return clean_tokens
 
 # load data
-engine = create_engine('sqlite:///../data/YourDatabaseName.db')
-df = pd.read_sql_table('YourTableName', engine)
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('InsertTableName', engine)
 
 # load model
-model = joblib.load("../models/your_model_name.pkl")
+model = joblib.load("../models/classifier.pkl")
 
 
 # index webpage displays cool visuals and receives user input text for model
@@ -39,13 +39,91 @@ model = joblib.load("../models/your_model_name.pkl")
 def index():
     
     # extract data needed for visuals
-    # TODO: Below is an example - modify to extract data for your own visuals
+    # Plot#1 - Show a number of messages of each genre
     genre_counts = df.groupby('genre').count()['message']
     genre_names = list(genre_counts.index)
     
+    # Plot#2 - Show a number of messages where the original language
+    # is english (null value in the 'original' column) or 
+    # non-english (the 'original' column is not null)
+    num_non_en = df['original'].isnull().sum()
+    num_en = df.shape[0] - num_non_en
+    
+    lang_names = ['English', 'Non-english']
+    lang_counts = [num_en, num_non_en]
+    
+    # Plot#3 & 4 - Show a number of message in each category and 
+    # use a color encoding for the genre
+    
+    # drop unused columns first
+    df_cat_genre = df.drop(['id', 'message', 'original'], axis=1)
+    
+    # For the 'related' column, replace 2 with 0 since 2 means not related
+    df_cat_genre['related'] = df_cat_genre['related'].replace(2, 0)
+    
+    # Get a list of category names sorted by a number of messages first!
+    df_cat = df_cat_genre.drop(['genre'], axis=1) 
+    cat_names = df_cat.sum().sort_values().index.tolist()
+    
+    # Then, group the dataframe by 'genre' and get a sum of each column
+    # since 1 means related and 0 means not related
+    df_cat_genre_count = df_cat_genre.groupby('genre').sum()
+    
+    # Ensure that the order of categories is sorted descendingly based on
+    # a number of messages
+    df_cat_genre_count = df_cat_genre_count[cat_names] 
+    cat_genre_names = df_cat_genre_count.columns.tolist()
+    
+    # Array to a plot object for plots 3 and 4
+    data_barplot = []
+    data_dotplot = []
+    
+    # Use the same color coding for both plots 3 and 4
+    genre_map = [
+        {'name': 'social', 'color': 'rgba(0, 164, 0, 1.0)'},
+        {'name': 'news', 'color': 'rgba(255, 118, 0, 1.0)'},
+        {'name': 'direct', 'color': 'rgba(0, 120, 185, 1.0)'}
+    ]
+    for cur_genre_map in genre_map:
+        # Get the current category name, color, and a number of messages
+        cur_genre = cur_genre_map['name']
+        cur_color = cur_genre_map['color']
+        cur_val = df_cat_genre_count.loc[cur_genre,:].values.tolist()
+        
+        # Plot#3 - Horizontal stacked bar to show a total number of message 
+        # in each category and use a color encoding for the genre
+        data_barplot.append(
+            Bar(
+                y = cat_genre_names,
+                x = cur_val,
+                name= cur_genre,
+                orientation = 'h',
+            )
+        )
+        
+        # Plot#4 - Dot plot to show a total number of message in each
+        # category and use a color encoding for the genre
+        data_dotplot.append(
+            Scatter(
+                y = cat_genre_names,
+                x = cur_val,
+                name= cur_genre,
+                mode='markers',
+                marker=dict(
+                    color=cur_color,
+                    line=dict(
+                        color=cur_color,
+                        width=1,
+                    ),
+                    symbol='circle',
+                    size=12,
+                )
+            )
+        )
+    
     # create visuals
-    # TODO: Below is an example - modify to create your own visuals
     graphs = [
+        # Plot#1 - Show a number of messages of each genre
         {
             'data': [
                 Bar(
@@ -63,7 +141,76 @@ def index():
                     'title': "Genre"
                 }
             }
+        },
+        
+        # Plot#2 - Show a number of messages where the original language
+        # is english or non-english
+        {
+            'data': [
+                Bar(
+                    x=lang_names,
+                    y=lang_counts
+                )
+            ],
+
+            'layout': {
+                'title': 'The number of messages based on original',
+                'yaxis': {
+                    'title': "Count"
+                }
+            }
+        },
+        
+        # Plot#3 - Horizontal stacked bar for a total number of message 
+        # in each category
+        {
+            'data': data_barplot,
+
+            'layout': {
+                'barmode': 'stack',
+                'title': 'Distribution of Message Categories by Genre',
+                'yaxis': {
+                    'dtick': 1
+                },
+                'xaxis': {
+                    'title': "The number of messages"
+                },
+                'height': 800,
+                'margin': dict(
+                    l=250,
+                    r=50,
+                    b=100,
+                    t=100,
+                    pad=4
+                ),
+            }
+        },
+        
+        # Plot#4 - Dot plot to show a total number of message in each
+        # category 
+        {
+            'data': data_dotplot,
+
+            'layout': {
+                'title': 'The number of messages of each category and genre',
+                'yaxis': {
+                    'dtick': 1
+                },
+                'xaxis': {
+                    'title': "The number of messages"
+                },
+                'height': 800,
+                'hovermode': 'closest',
+                'margin': dict(
+                    l=250,
+                    r=50,
+                    b=100,
+                    t=100,
+                    pad=4
+                ),
+            }
         }
+        
     ]
     
     # encode plotly graphs in JSON
